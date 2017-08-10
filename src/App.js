4a3a0ca0406750e97805673053be94abe22c25b4
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-10 10:05:17 -0400

    Create flux files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Grid>
+          <Row>
+            <Col xs={12} md={12} lg={12}>
+            hi
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
---
+++ src/dispatcher/AppDispatcher.js
@@ -0,0 +1,5 @@
+import {Dispatcher} from 'flux';
+
+const AppDisPatcher = new Dispatcher();
+
+export default AppDisPatcher;
