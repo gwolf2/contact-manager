e6f279c5f712ac8ba74cb5fb8e7c24db839c6a20
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-10 10:24:56 -0400

    Complete flux scaffolding

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,5 +1,22 @@
 import AppDispatcher from '../dispatcher/AppDispatcher';
+import AppConstants from '../constants/AppConstants';
+import ContactsAPI from '../utils/ContactsAPI';
 
 export default {
-
+  receiveContacts: () => {
+    ContactsAPI
+      .getContacts('https://jsonplaceholder.typicode.com/users')
+      .then(contacts => {
+        AppDispatcher.dispatch({
+          actionType: AppConstants.RECEIVE_CONTACTS,
+          contacts: contacts
+        })
+      })
+      .catch(message => {
+        AppDispatcher.dispatch({
+          actionType: AppConstants.RECEIVE_CONTACTS_ERROR,
+          message: message
+        })
+      });
+  }
 }
