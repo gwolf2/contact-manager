9801fec8cb84e1c3665a55bf6347271b944c07ed
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-10 11:46:08 -0400

    Add contact form

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -18,5 +18,22 @@ export default {
           message: message
         })
       });
-  }
+  },
+
+  saveContact: (contact) => {
+    ContactsAPI
+      .saveContact('https://jsonplaceholder.typicode.com/users', contact)
+      .then(contact => {
+        AppDispatcher.dispatch({
+          actionType: AppConstants.RECEIVE_CONTACT,
+          contact: contact
+        })
+      })
+      .catch(message => {
+        AppDispatcher.dispatch({
+          actionType: AppConstants.RECEIVE_CONTACT_ERROR,
+          message: message
+        })
+      });
+  },
 }
